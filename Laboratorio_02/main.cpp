3f367bfa81f080feaa11346bcd84dd49fe946702
MisaelVM <mvilchezmo@unsa.edu.pe>
2022-05-03 00:43:52 -0500

    Laboratorio 02

---
+++ Laboratorio_02/main.cpp
@@ -0,0 +1,41 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main()
+{
+	// Initialization
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Window creation
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Laboratorio 2", nullptr, nullptr);
+	if (!window) {
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	// Render loop
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Terminate
+	glfwTerminate();
+	return 0;
+}
